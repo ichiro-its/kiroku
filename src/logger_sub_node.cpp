904dc6ca5aaa2a15228f3562141b6a426951bf4e
azharanas17 <99221296+azharanas17@users.noreply.github.com>
2022-03-31 13:26:56 +0700

    Update logger_sub_node.cpp

--- src/logger_sub_node.cpp
+++ src/logger_sub_node.cpp
@@ -7,14 +7,12 @@
 using std::placeholders::_1;
 #include "rclcpp/rclcpp.hpp"
 #include "first_interfaces/msg/log.hpp"
-// #include "first_pubsub/logger.hpp"
 #include "first_pubsub/logger_sub_node.hpp"
 
 using json = nlohmann::json;
 using std::placeholders::_1;
 
 enum LogLevel { DEBUG, INFO, WARN, ERROR, FATAL };
-// int array_available = 0;
 json jsn_lg = {};
 
     LoggerSub::LoggerSub()
@@ -41,19 +39,11 @@ json jsn_lg = {};
             std::ofstream file_log;
             file_log.open("src/first_pubsub/" + msg->filename + ".log", std::ios_base::trunc | std::ios_base::out);
 
-            
-            // if(!array_available) {
-            //     json_log = json::array();
-            //     array_available = 1;
-            // }
-
             jsn_lg += {
                 {"message_log", msg->message_log},
                 {"level", msg->level},
                 {"time", msg->time}
             };
-
-            // json_log += jsn_lg;
 
             file_log << jsn_lg.dump(0);
 
